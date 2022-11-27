59f72a23daf161f47c1de58b7062e063c1ef4cca
minkman <lomick2090@gmail.com>
2022-11-27 06:58:49 -0600

    Finish card text layout

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const pagesBox = document.getElementById('pages');
 const readBox = document.getElementById('read');
 const addButton = document.getElementById('add');
 
+//constructor for new books
 function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -15,6 +16,8 @@ function book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
+
+    //stores and deletes input instead of using the traditional 'submit' button
     let inputTitle = titleBox.value;
     titleBox.value = '';
     let inputAuthor = authorBox.value;
@@ -29,6 +32,66 @@ function addBookToLibrary() {
     myLibrary.push(newBook);
     addCard();
 }
+
+function addCard() {
+    let card = document.createElement('div');
+    var i = myLibrary.length - 1;
+
+    //data-index is used later in other functions to connect the DOM elements to their corresponding data in myLibrary array
+    card.setAttribute('data-index', i);
+    card.className = 'card';
+
+    let newTitle = myLibrary[i].title;
+    let cardTitle = document.createElement('h1');
+    cardTitle.className = 'title';
+    cardTitle.innerHTML = newTitle;
+
+    let newAuthor = myLibrary[i].author;
+    let cardAuthor = document.createElement('h2');
+    cardAuthor.className = 'author';
+    cardAuthor.innerHTML = `By: ${newAuthor}`;
+
+    let newPages = myLibrary[i].pages;
+    let cardPages = document.createElement('h2');
+    cardPages.className = 'pages';
+    cardPages.innerHTML = `Page #: ${newPages}`;
+
+    let hasRead = myLibrary[i].read;
+    let cardRead = document.createElement('input');
+    cardRead.id = 'read';
+    cardRead.className = 'readbox';
+    cardRead.type = 'checkbox'; 
+    if (hasRead) {
+        cardRead.checked = true
+    } else {
+        cardRead.checked = false;
+    }
+
+    let labelRead = document.createElement('label')
+    labelRead.className = 'readlabel';
+    labelRead.id = 'read';
+    labelRead.innerHTML = 'Read?';
+
+    let xButton = document.createElement('button');
+    xButton.innerHTML = 'X';
+    xButton.className = 'exitbutton';
+
+    card.appendChild(cardTitle);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardPages);
+    labelRead.appendChild(cardRead);
+    card.appendChild(labelRead);
+    card.appendChild(xButton);
+    cardHolder.appendChild(card);
+
+    let exitButtons = document.getElementsByClassName('exitbutton');
+    let readChecks = document.getElementsByClassName('readbox');
+
+    readChecks[readChecks.length - 1].addEventListener('click', toggleRead);
+    exitButtons[exitButtons.length - 1].addEventListener('click', deleteBook);
+}
+
+
 function deleteBook() {
     var index = this.parentNode.getAttribute('data-index');
     myLibrary.splice(index, 1);
@@ -50,57 +113,4 @@ function toggleRead () {
     }
 }
 
-
-
 addButton.addEventListener('click', addBookToLibrary);
-
-function addCard() {
-    let card = document.createElement('div');
-    let i = myLibrary.length - 1;
-    card.setAttribute('data-index', i);
-    card.className = 'card';
-
-    let newTitle = myLibrary[i].title;
-    let cardTitle = document.createElement('h1');
-    cardTitle.className = 'title';
-    cardTitle.innerHTML = newTitle;
-
-    let newAuthor = myLibrary[i].author;
-    let cardAuthor = document.createElement('h2');
-    cardAuthor.className = 'author';
-    cardAuthor.innerHTML = `By: ${newAuthor}`;
-
-    let newPages = myLibrary[i].pages;
-    let cardPages = document.createElement('h2');
-    cardPages.className = 'pages';
-    cardPages.innerHTML = `Page #: ${newPages}`;
-
-    let hasRead = myLibrary[i].read;
-    let cardRead = document.createElement('input');
-    cardRead.className = 'readbox';
-    cardRead.type = 'checkbox'; 
-    if (hasRead) {
-        cardRead.checked = true
-    } else {
-        cardRead.checked = false;
-    }
-
-    let xButton = document.createElement('button');
-    xButton.innerHTML = 'X';
-    xButton.className = 'exitbutton';
-
-    card.appendChild(cardTitle);
-    card.appendChild(cardAuthor);
-    card.appendChild(cardPages);
-    card.appendChild(cardRead);
-    card.appendChild(xButton);
-    cardHolder.appendChild(card);
-
-    const exitButtons = document.getElementsByClassName('exitbutton');
-    const readChecks = document.getElementsByClassName('readbox');
-
-    readChecks[readChecks.length - 1].addEventListener('click', toggleRead);
-    exitButtons[exitButtons.length - 1].addEventListener('click', deleteBook);
-}
-
-
